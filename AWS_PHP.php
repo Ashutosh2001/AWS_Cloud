35f18f7feb9093ce311235e9baece030ad044e5c
Ashutosh Saini <55857615+Ashutosh2001@users.noreply.github.com>
2020-06-15 14:50:46 +0530

    comment

--- AWS_PHP.php
+++ AWS_PHP.php
@@ -2,4 +2,5 @@
 print "<h2>PHP is Fun!</h2>";
 print "Hello world!<br>";
 print "I'm about to learn PHP!";
+print "<img src='https://github.com/Ashutosh2001/Cloud_Image.git' width='300' height='140' />"
 ?>
