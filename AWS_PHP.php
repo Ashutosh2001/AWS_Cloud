eb3c1022c557126142add4182eb00fc849214a7f
Ashutosh Saini <55857615+Ashutosh2001@users.noreply.github.com>
2020-06-16 12:56:27 +0530

    Update AWS_PHP.php

--- AWS_PHP.php
+++ AWS_PHP.php
@@ -1,6 +1,6 @@
 <?php
-print "<h2>PHP is Fun!</h2>";
-print "Hello world!<br>";
-print "I'm about to learn PHP!";
-print "<img src='D:\PICS\93808' width='300' height='140' />"
+print ("<h2>PHP is Fun!</h2>)";
+print ("Hello world!<br>");
+print ("I'm about to learn PHP!");
+print ("<img src='D:\PICS\93808' width='300' height='140' />")
 ?>
