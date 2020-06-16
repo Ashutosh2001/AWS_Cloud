9de32e85b5c4492e5c58495d9ea370c0fb59e7b8
Ashutosh Saini <55857615+Ashutosh2001@users.noreply.github.com>
2020-06-16 12:48:39 +0530

    Update AWS_PHP.php

--- AWS_PHP.php
+++ AWS_PHP.php
@@ -2,5 +2,5 @@
 print "<h2>PHP is Fun!</h2>";
 print "Hello world!<br>";
 print "I'm about to learn PHP!";
-print "<img src='https://github.com/Ashutosh2001/AWS_Cloud/blob/master/93863.jpg' width='300' height='140' />"
+print "<img src='D:\PICS\93808' width='300' height='140' />"
 ?>
